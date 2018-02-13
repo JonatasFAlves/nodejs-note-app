6503d540123b3bc683db07d9a5bd908fefb5bfb8
JonatasFAlves <alves.jnts@gmail.com>
2018-02-13 19:32:05 -0300

    Removed console.log statements

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-console.log('Starting app');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
-console.log('');
 
 // Mapping user input:
 //   argv will have acess to full user input
